e423218bb4afe5aa0940261cb83d5ad46cb17f1a
Adama Camara <48602776+acamara2016@users.noreply.github.com>
2024-01-10 13:26:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 # Define the variable
 variable "branch_name" {}
+variable "image_tag" {}
 
 # Output the value of the TF_VAR_branch_name variable
 output "output_branch_name" {
   value = var.branch_name
+}
+
+output "output_image_tag" {
+  value = var.image_tag
 }
